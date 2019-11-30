9cedaa1dd7d689611bf1af9df03daf2eeb2f1a3b
Anest <anest@66ws.cc>
2019-11-30 08:09:12 -0400

    add command tip,can open or close ls

--- golang/ftpserve.go
+++ golang/ftpserve.go
@@ -4,13 +4,38 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 )
 
+var (
+	ls_open = false
+)
+
 func main() {
+	p := false
+	if len(os.Args) > 1 {
+		for _, v := range os.Args {
+			switch v {
+			case "h", "-h", "help":
+				fmt.Println("h  - show this help\nv  - get version\nls  - open ls function.")
+				os.Exit(0)
+			case "v", "-v", "version":
+				fmt.Println("11/30/2019 Sat")
+				os.Exit(0)
+			case "ls", "-ls":
+				p = true
+				ls_open = true
+			}
+		}
+		if !p {
+			fmt.Println("Do you mean \"-h\" ?")
+			os.Exit(0)
+		}
+	}
 	e := echo.New()
-	e.GET("/index", index)
+	e.GET("/", index)
 	e.GET("/download/*", getfile)
 	e.GET("/ls/*", getls)
 	e.GET("/version", getversion)
@@ -18,11 +43,11 @@ func main() {
 }
 
 func getversion(c echo.Context) error {
-	return c.String(http.StatusOK, "06/08/2019 Sat")
+	return c.String(http.StatusOK, "11/30/2019 Sun")
 }
 
 func index(c echo.Context) error {
-	return c.String(http.StatusOK, "It's the file download server.<br>You can use the path to download the file on the machine.")
+	return c.String(http.StatusOK, "It's the file download server.\nYou can use the path to download the file on the machine.")
 }
 
 func getfile(c echo.Context) error {
@@ -31,9 +56,14 @@ func getfile(c echo.Context) error {
 }
 
 func getls(c echo.Context) error {
-	files := getls_getfiles(c.Request().URL.Path[4:])
-	fmt.Println(c.Request().URL.Path[4:])
-	return c.String(http.StatusOK, files)
+	if ls_open {
+		files := getls_getfiles(c.Request().URL.Path[4:])
+		fmt.Println(c.Request().URL.Path[4:])
+		return c.String(http.StatusOK, files)
+	} else {
+		return c.String(http.StatusNotImplemented, "Error 501")
+	}
+
 }
 func getls_getfiles(path string) string {
 	skillfolder := path
@@ -50,3 +80,14 @@ func getls_getfiles(path string) string {
 	result = result + ds + "\n" + fs
 	return result
 }
+
+// func Exists(path string) bool {
+// 	_, err := os.Stat(path)
+// 	if err != nil {
+// 		if os.IsExist(err) {
+// 			return true
+// 		}
+// 		return false
+// 	}
+// 	return true
+// }
