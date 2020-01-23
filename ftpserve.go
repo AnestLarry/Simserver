6e33060372242dd7288d4444c141be0467c2b34d
Anest <anest@66ws.cc>
2020-01-23 09:29:43 -0400

    add dls function

--- ftpserve.go
+++ ftpserve.go
@@ -10,7 +10,9 @@ import (
 )
 
 var (
-	ls_open = false
+	ls_open  = false
+	dls_open = false
+	Version  = "Jan 23,2020 Thu."
 )
 
 func main() {
@@ -19,14 +21,17 @@ func main() {
 		for _, v := range os.Args {
 			switch v {
 			case "h", "-h", "help":
-				fmt.Println("h  - show this help\nv  - get version\nls  - open ls function.")
+				fmt.Println(" h  - show this help\n v  - get version\n ls  - open ls function\n dls  - download with ls list(auto open ls function).")
 				os.Exit(0)
 			case "v", "-v", "version":
-				fmt.Println("11/30/2019 Sat")
+				fmt.Println(Version)
 				os.Exit(0)
 			case "ls", "-ls":
 				p = true
 				ls_open = true
+			case "dls", "-dls":
+				p = true
+				dls_open = true
 			}
 		}
 		if !p {
@@ -38,12 +43,13 @@ func main() {
 	e.GET("/", index)
 	e.GET("/download/*", getfile)
 	e.GET("/ls/*", getls)
+	e.GET("/dls/*", getdls)
 	e.GET("/version", getversion)
 	e.Logger.Fatal(e.Start(":5000"))
 }
 
 func getversion(c echo.Context) error {
-	return c.String(http.StatusOK, "11/30/2019 Sun")
+	return c.String(http.StatusOK, Version)
 }
 
 func index(c echo.Context) error {
@@ -58,13 +64,14 @@ func getfile(c echo.Context) error {
 func getls(c echo.Context) error {
 	if ls_open {
 		files := getls_getfiles(c.Request().URL.Path[4:])
+
 		fmt.Println(c.Request().URL.Path[4:])
 		return c.String(http.StatusOK, files)
 	} else {
 		return c.String(http.StatusNotImplemented, "Error 501")
 	}
-
 }
+
 func getls_getfiles(path string) string {
 	skillfolder := path
 	result := "Items:\n\n"
@@ -78,6 +85,34 @@ func getls_getfiles(path string) string {
 		}
 	}
 	result = result + ds + "\n" + fs
+	return result
+}
+
+func getdls(c echo.Context) error {
+	if dls_open {
+		files := getdls_getfiles(c.Request().URL.Path[5:])
+		fmt.Println(c.Request().URL.Path[5:])
+		return c.HTML(http.StatusOK, files)
+	} else {
+		return c.String(http.StatusNotImplemented, "Error 501")
+	}
+}
+
+func getdls_getfiles(path string) string {
+	skillfolder := path
+	result := "<html>Items:<br><br>"
+	fs, ds := "<p> File:<br>", "<p> Dir:\n"
+	files, _ := ioutil.ReadDir(skillfolder)
+	for _, file := range files {
+		if file.IsDir() {
+			ds += "  " + file.Name() + "<br>"
+		} else {
+			fs += "  <a href='/download/" + path + file.Name() + "'>" + file.Name() + "</a><br>"
+		}
+	}
+	ds += "</p>"
+	fs += "</p>"
+	result = result + ds + "<br>" + fs + "</html>"
 	return result
 }
 
