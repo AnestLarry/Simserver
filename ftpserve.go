e960d4ceab981b156f964352675ad08500bade1b
Anest <32736719+AnestLarry@users.noreply.github.com>
2023-03-26 04:09:40 -0400

    refactor(cli): using flag to parse flag

--- ftpserve.go
+++ ftpserve.go
@@ -7,6 +7,7 @@ import (
 	"Simserver/uploadGroup"
 	"Simserver/viewGroup"
 	"embed"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -37,118 +38,16 @@ var (
 )
 
 func main() {
-	var err error
-	if len(os.Args) > 1 {
-	ArgsFor:
-		for i := 1; i < len(os.Args); i++ {
-			switch os.Args[i] {
-			case "h", "-h", "help":
-				fmt.Println(`Tips:
- h  - show this help
- v  - get version
-Mode:
- ls  - open ls function
- upload  - allow user upload files to host
- uploadText  - allow user fill textarea to save text in txt
- zip  - allow zip dir for downloadGroup (DANGER!)
- https  - use https with crt and key 
- log  - put log in file
- downloadCode  - use downloadGroup code to downloadGroup a group file with setting
- view  - use view in running
- login  - add account password auth for all resources
-Args:
- p / port  - use the port
- ip  - use the ip
- config  - use 'config.json' args
-Task:
- RFN  - restore files' name`)
-				os.Exit(0)
-			case "v", "-v", "version":
-				fmt.Println(Version)
-				os.Exit(0)
-			case "ls", "-ls":
-				fmt.Println(" -  ls mode on.")
-				downloadGroup.Ls_open = true
-			case "ip", "-ip":
-				i++
-				ip = os.Args[i]
-			case "p", "-p", "port", "-port":
-				i++
-				port = os.Args[i]
-			case "upload", "-upload":
-				fmt.Println(" -  upload mode on.")
-				uploadGroup.Upload_open = true
-				if Libs.LibsXExists("./upload") {
-					if !Libs.LibsXIsDir("./upload") {
-						fmt.Println("upload is not a folder!")
-						os.Exit(0)
-					}
-				} else {
-					os.Mkdir("./upload", 0764)
-				}
-			case "uploadText", "-uploadText", "uT", "-uT":
-				fmt.Println(" -  upload text mode on.")
-				uploadGroup.Upload_text_open = true
-				if Libs.LibsXExists("./upload") {
-					if !Libs.LibsXIsDir("./upload") {
-						fmt.Println("upload is not a folder!")
-						os.Exit(0)
-					}
-				} else {
-					os.Mkdir("./upload", 0764)
-				}
-			case "zip", "-zip":
-				fmt.Println(" -  zip mode on.")
-				downloadGroup.Zip_open = true
-			case "downloadCode", "dC", "-downloadCode", "-dC":
-				fmt.Println(" -  downloadCode mode on.")
-				downloadGroup.DownloadCode_open = true
-			case "view", "-view":
-				fmt.Println(" -  view mode on.")
-				viewGroup.View_open = true
-			case "log", "-log":
-				fmt.Println(" -  log file mode on.")
-				log_file_open = true
-			case "https", "-https":
-				https_open = true
-				pem_file = os.Args[i+1]
-				key_file = os.Args[i+2]
-				i = i + 2
-				fmt.Println(" -  https mode on.")
-			case "RFN", "-RFN":
-				restoreFileName()
-			case "config", "-config":
-				fmt.Println("start parse 'config.json'")
-				loadConfigFromArgsConfigStruct(argsConfig.ArgConfigInit())
-				break ArgsFor
-			case "login", "-login":
-				fmt.Println(" -  login mode on.")
-				login.open = true
-				login.account = os.Args[i+1]
-				login.password = os.Args[i+2]
-				i += 2
-			default:
-				fmt.Println("There is arg(s) cannot parse. Do you need \"-h\" ?")
+	parseArgs()
+	if uploadGroup.Upload_open || uploadGroup.Upload_text_open {
+		if Libs.LibsXExists("./upload") {
+			if !Libs.LibsXIsDir("./upload") {
+				fmt.Println("upload is not a folder!")
 				os.Exit(0)
 			}
+		} else {
+			os.Mkdir("./upload", 0764)
 		}
-	}
-	if log_file_open {
-		gin.DisableConsoleColor()
-		var f *os.File
-		var err error
-		if !Libs.LibsXExists("ftps.log") {
-			f, err = os.Create("ftps.log")
-			if err != nil {
-				panic(err)
-			}
-		} else if Libs.LibsXIsFile("ftps.log") {
-			f, err = os.OpenFile("ftps.log", os.O_APPEND, os.ModePerm)
-			if err != nil {
-				panic(err)
-			}
-		}
-		gin.DefaultWriter = io.MultiWriter(f)
 	}
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
@@ -158,9 +57,6 @@ Task:
 			"Latency": param.Latency, "User-Agent": param.Request.UserAgent(), "ErrorMessage": param.ErrorMessage}
 		return fmt.Sprintf("%v\n", r)
 	}))
-	if login.open {
-		r.Use(gin.BasicAuth(gin.Accounts{login.account: login.password}))
-	}
 	r.Use(gin.Recovery())
 	r.GET("/version", func(c *gin.Context) {
 		c.JSON(200, gin.H{"version": Version})
@@ -173,6 +69,7 @@ Task:
 	})
 	routerGroup_init(r)
 	fmt.Printf("%s\n%s:%s\n", strings.Repeat("-", 15), ip, port)
+	var err error
 	if https_open {
 		err = r.RunTLS(fmt.Sprintf("%s:%s", ip, port), pem_file, key_file)
 	} else {
@@ -251,6 +148,66 @@ func restoreFileName() {
 		}
 	}
 	os.Exit(0)
+}
+
+func parseArgs() {
+	httpsArg := ""
+	loginArg := ""
+	flag.Func("version", "show the version", func(s string) error {
+		fmt.Println(Version)
+		os.Exit(0)
+		return nil
+	})
+	flag.BoolVar(&downloadGroup.Ls_open, "ls", false, "open ls mode")
+	flag.StringVar(&ip, "ip", "0.0.0.0", "set the ip listen")
+	flag.StringVar(&port, "port", "5000", "set the port listen")
+	flag.BoolVar(&uploadGroup.Upload_open, "upload", false, "open upload mode")
+	flag.BoolVar(&uploadGroup.Upload_text_open, "uT", false, "open upload_text mode")
+	flag.BoolVar(&downloadGroup.Zip_open, "zip", false, "open zip mode")
+	flag.BoolVar(&downloadGroup.DownloadCode_open, "dC", false, "open download_code mode")
+	flag.BoolVar(&viewGroup.View_open, "view", false, "open view mode")
+	flag.Func("log", "open log file", func(s string) error {
+		log_file_open = true
+		gin.DisableConsoleColor()
+		var f *os.File
+		var err error
+		if !Libs.LibsXExists("ftps.log") {
+			f, err = os.Create("ftps.log")
+			if err != nil {
+				panic(err)
+			}
+		} else if Libs.LibsXIsFile("ftps.log") {
+			f, err = os.OpenFile("ftps.log", os.O_APPEND, os.ModePerm)
+			if err != nil {
+				panic(err)
+			}
+		}
+		gin.DefaultWriter = io.MultiWriter(f)
+		return nil
+	})
+	flag.StringVar(&httpsArg, "https", "", "use HTTPS with cer and key\nexample: \"cer.cer key.pvk\"")
+	flag.Func("RFN", "restore files' name", func(s string) error {
+		restoreFileName()
+		os.Exit(0)
+		return nil
+	})
+	flag.Func("config", "use 'config.json' args", func(s string) error {
+		loadConfigFromArgsConfigStruct(argsConfig.ArgConfigInit())
+		return nil
+	})
+	flag.StringVar(&loginArg, "login", "", "add account password auth for all resource.\nexample: \"admin admin\"")
+	flag.Parse()
+	if httpsArg != "" {
+		https := strings.Split(httpsArg, " ")
+		https_open = true
+		pem_file, key_file = https[0], https[1]
+	}
+	if loginArg != "" {
+		fmt.Printf("login [%s]\n", loginArg)
+		loginArgs := strings.Split(loginArg, " ")
+		login.open = true
+		login.account, login.password = loginArgs[0], loginArgs[1]
+	}
 }
 
 func router_init(r *gin.Engine) {
